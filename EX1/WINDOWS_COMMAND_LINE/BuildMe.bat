49853198264f74e6a30a47760a4e49974c79a25e
OrenIshShalom <tuna.is.good.for.you@gmail.com>
2016-11-20 14:23:32 +0200

    Add JFlex synthesis of Lexer.java from LEX_FILE.lex

--- EX1/WINDOWS_COMMAND_LINE/BuildMe.bat
+++ EX1/WINDOWS_COMMAND_LINE/BuildMe.bat
@@ -1,8 +1,27 @@
 @echo.
+@echo ############################################################
+@echo #                                                          #
+@echo #                                                          #
+@echo # [0] Clean *.class files and machine generated Lexer.java #
+@echo #                                                          #
+@echo #                                                          #
+@echo ############################################################
+del /q FOLDER_1_src\Lexer.java FOLDER_2_bin\*.class 2>nul
+@echo.
+@echo ############################################################
+@echo #                                                          #
+@echo #                                                          #
+@echo # [1] Use JFlex to synthesize Lexer.java from LEX_FILE.lex #
+@echo #                                                          #
+@echo #                                                          #
+@echo ############################################################
+call jflex.bat -q -d FOLDER_1_src FOLDER_0_lex/LEX_FILE.lex
+@echo on
+@echo.
 @echo ########################################################
 @echo #                                                      #
 @echo #                                                      #
-@echo # [1] Create *.class files from *.java files + CUP JAR #
+@echo # [2] Create *.class files from *.java files + CUP JAR #
 @echo #                                                      #
 @echo #                                                      #
 @echo ########################################################
@@ -11,7 +30,7 @@ javac -cp ./FOLDER_3_CUP_JARS/java-cup-11b-runtime.jar -d FOLDER_2_bin FOLDER_1_
 @echo ###########################################################
 @echo #                                                         #
 @echo #                                                         #
-@echo # [2] Create a JAR file from from *.class files + CUP JAR #
+@echo # [3] Create a JAR file from from *.class files + CUP JAR #
 @echo #                                                         #
 @echo #                                                         #
 @echo ###########################################################
@@ -20,7 +39,7 @@ jar cfm LEXER.jar ./FOLDER_7_MANIFEST/MANIFEST.MF -C ./FOLDER_2_bin/ .
 @echo #############################
 @echo #                           #
 @echo #                           #
-@echo # [3] Run resulting program #
+@echo # [4] Run resulting program #
 @echo #                           #
 @echo #                           #
 @echo #############################
