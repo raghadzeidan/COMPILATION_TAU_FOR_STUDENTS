d9df73bb6853809a5f6440fd39654ead1b0b6257
OrenIshShalom <tuna.is.good.for.you@gmail.com>
2016-11-20 08:37:20 +0200

    Updated version

--- EX1/WINDOWS_COMMAND_LINE/BuildMe.bat
+++ EX1/WINDOWS_COMMAND_LINE/BuildMe.bat
@@ -1,3 +1,3 @@
-javac -cp ./CUP_JARS/java-cup-11b-runtime.jar -d bin src/*.java
-jar cvfm LEXER.jar ./MANIFEST/MANIFEST.MF -C ./bin/ .
-java -jar LEXER.jar ./Input/Input.ic ./Output/OutputTokens.txt
+javac -cp ./FOLDER_3_CUP_JARS/java-cup-11b-runtime.jar -d bin src/*.java
+jar cvfm LEXER.jar ./FOLDER_7_MANIFEST/MANIFEST.MF -C ./FOLDER_2_bin/ .
+java -jar LEXER.jar ./FOLDER_4_Input/Input.ic ./FOLDER_5_Output/OutputTokens.txt
