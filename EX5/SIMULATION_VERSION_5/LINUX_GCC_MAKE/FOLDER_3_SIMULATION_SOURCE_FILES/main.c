0bc2930db31e49e0aaca1964281473881fa80042
OrenGitHub <tuna.is.good.for.you@gmail.com>
2017-02-09 14:48:01 +0200

    ...

---
+++ EX5/SIMULATION_VERSION_5/LINUX_GCC_MAKE/FOLDER_3_SIMULATION_SOURCE_FILES/main.c
@@ -0,0 +1,20 @@
+/*********************/
+/* FILE NAME: main.c */
+/*********************/
+
+/*************************/
+/* GENERAL INCLUDE FILES */
+/*************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*************************/
+/* PROHECT INCLUDE FILES */
+/*************************/
+#include "Pseudo_Assembley_To_Simulation.h"
+
+int main(int argc, char **argv)
+{
+	Pseudo_Assembley_To_Simulation(argv[1],argv[2]);
+}
---
+++ EX5/SIMULATION_VERSION_5/LINUX_GCC_MAKE/FOLDER_3_SIMULATION_SOURCE_FILES/util.c
@@ -0,0 +1,50 @@
+/*********************/
+/* FILE NAME: util.c */
+/*********************/
+
+/****************************************/
+/* WARNING DISABLE: sprintf - I love it */
+/****************************************/
+#pragma warning (disable: 4996)
+
+/*************************/
+/* GENERAL INCLUDE FILES */
+/*************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*****************/
+/* INCLUDE FILES */
+/*****************/
+#include "util.h"
+
+void *checked_malloc(int len)
+{
+	void *p = malloc(len);
+	if (!p)
+	{
+		fprintf(stderr,"\nRan out of memory!\n");
+		exit(1);
+	}
+	
+	return p;
+}
+
+string String(char *s)
+{
+	string p = (string) checked_malloc(strlen(s)+1);
+	strcpy(p,s);
+	
+	if (strcmp(s,"789") == 0)
+	{
+		int fff=0;
+	}
+
+	if (strcmp(s,"-789") == 0)
+	{
+		int fff=0;
+	}
+	
+	return p;
+}
