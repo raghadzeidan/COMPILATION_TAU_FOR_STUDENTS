b8a6cabea76835fa1d11a37d241e1e8c52c91797
OrenGitHub <tuna.is.good.for.you@gmail.com>
2018-12-24 06:32:48 +0200

    ...

--- FOLDER_1_TIRGULIM/SLIDES_04_IR/EXAMPLES/example_03.c
+++ FOLDER_1_TIRGULIM/SLIDES_04_IR/EXAMPLES/example_03.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-int foo(int n)
+typedef struct P {
+	int ID;
+	int age;
+	int height;
+	int weight;
+} P;
+
+int main()
 {
-    int sum=17;
-    for (int i=0;i<n;i++)
-    {
-        sum=sum+i+46;
-    }
-    return sum;
-}
-int main(int argc, char **argv) {
-    return printf("%d\n",foo(argc));
+	P *p;
+	p->weight = 78;
+	return p->age;
 }
